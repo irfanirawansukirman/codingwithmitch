622fbf1dd7dd478fc11b297131cbec60cb9ebccf
irfanirawansukirman <dadang.kotz@gmail.com>
2019-08-16 14:07:28 +0700

    coroutines beginner example

---
+++ android-kotlin-coroutines-beginner/app/src/main/java/com/irfanirawansukirman/cwm_kotlincoroutinesbeginner/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.irfanirawansukirman.cwm_kotlincoroutinesbeginner
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    private val RESULT_1 = "Result #1"
+    private val RESULT_2 = "Result #2"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            CoroutineScope(IO).launch {
+                fakeApiRequest()
+            }
+        }
+    }
+
+    private fun setNewText(input: String) {
+        val newText = text.text.toString() + "\n$input"
+        text.text = newText
+    }
+
+    private suspend fun setTextOnMainThread(newText: String) {
+        // this function will switch current coroutines dispatcher IO to dispatcher MAIN
+        withContext(Main) {
+            setNewText(newText)
+        }
+    }
+
+    private suspend fun fakeApiRequest() {
+        val result1 = getResult1FromApi()
+        println("debug: $result1")
+        setTextOnMainThread(result1)
+
+        val result2 = getResult2FromApi()
+        setTextOnMainThread(result2)
+    }
+
+    private suspend fun getResult1FromApi(): String {
+        logThread("getResult1FromApi")
+        delay(1000)
+        return RESULT_1
+    }
+
+    private suspend fun getResult2FromApi(): String {
+        logThread("getResult2FromApi")
+        delay(1000)
+        return RESULT_2
+    }
+
+    private fun logThread(methodName: String) {
+        println("debug: ${methodName}: ${Thread.currentThread().name}")
+    }
+
+    /**
+     * reference: {
+     * suspend function => https://stackoverflow.com/questions/47871868/what-does-suspend-function-mean-in-kotlin-coroutine
+     */
+
+}
